aee3c129f38bb86b31d4835cbc1f76eb6cea6c2f
jason-nn <jason.ho@obf.ateneo.edu>
2021-10-20 23:19:47 +0800

    added 1.0_slide22.rb

---
+++ 1.0_slide22.rb
@@ -0,0 +1,17 @@
+def is_perfect_square(num)
+    count = 0
+    square = 0
+    while square <= num
+        return true if (square == num)
+        count += 1
+        square = count**2
+    end
+    return false
+end
+
+puts(is_perfect_square(-1))
+puts(is_perfect_square(0))
+puts(is_perfect_square(3))
+puts(is_perfect_square(4))
+puts(is_perfect_square(25))
+puts(is_perfect_square(26))
