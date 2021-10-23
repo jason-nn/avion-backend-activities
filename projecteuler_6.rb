b52360b260b7ce44e6f4e6605662a5c850fa2766
jason-nn <jason.ho@obf.ateneo.edu>
2021-10-23 12:41:19 +0800

    added projecteuler_6.rb

---
+++ projecteuler_6.rb
@@ -0,0 +1,56 @@
+# ----- v1 ----- #
+
+def sum_of_squares(i, j)
+    output = 0
+
+    for k in i..j
+        output += k**2
+    end
+
+    return output
+end
+
+def square_of_sum(i, j)
+    output = 0
+
+    for k in i..j
+        output += k
+    end
+
+    output **= 2
+
+    return output
+end
+
+def difference(i, j)
+    return square_of_sum(i, j) - sum_of_squares(i, j)
+end
+
+puts difference(1, 10)
+puts difference(1, 100)
+
+# ----- v2 ----- #
+
+def sum_of_squares(i, j)
+    return (i..j).to_a.map { |x| x**2 }.sum
+end
+
+def square_of_sum(i, j)
+    return ((i + j) * (-i + j + 1) / 2)**2
+end
+
+def difference(i, j)
+    return square_of_sum(i, j) - sum_of_squares(i, j)
+end
+
+puts difference(1, 10)
+puts difference(1, 100)
+
+# ----- v3 ----- #
+
+def difference(i, j)
+    (((i + j) * (-i + j + 1) / 2)**2) - ((i..j).to_a.map { |x| x**2 }.sum)
+end
+
+puts difference(1, 10)
+puts difference(1, 100)
