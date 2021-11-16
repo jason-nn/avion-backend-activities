2dc0419781049d61aeb0f8cdd45e4995484f9726
jason-nn <jason.ho@obf.ateneo.edu>
2021-11-16 19:43:10 +0800

    edit

--- 4.3_slide2.rb
+++ 4.3_slide2.rb
@@ -1,19 +1,23 @@
 def encode(input)
-    input
-        .gsub('a', '1')
-        .gsub('e', '2')
-        .gsub('i', '3')
-        .gsub('o', '4')
-        .gsub('u', '5')
+    input.gsub(
+        /[aeiou]/,
+        'a' => '1',
+        'e' => '2',
+        'i' => '3',
+        'o' => '4',
+        'u' => '5',
+    )
 end
 
 def decode(input)
-    input
-        .gsub('1', 'a')
-        .gsub('2', 'e')
-        .gsub('3', 'i')
-        .gsub('4', 'o')
-        .gsub('5', 'u')
+    input.gsub(
+        /[aeiou]/,
+        '1' => 'a',
+        '2' => 'e',
+        '3' => 'i',
+        '4' => 'o',
+        '5' => 'u',
+    )
 end
 
 puts encode('hello')
